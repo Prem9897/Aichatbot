b420797ef4c325ad1ab4846b3b6cc759b1337cfb
Prem mishra <118355053+Prem9897@users.noreply.github.com>
2022-11-20 20:56:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
-api_hash = ""
+api_hash = "bb0ddae0921fc020ce61faae2d1261d5"
 
-api_id = int("")
+api_id = int("4277083")
 
-bot_token = ""
+bot_token = "5545963604:AAFaDB-N19p1c9mPBruvzScQPdiJqM-7x30"
 
-luna_key = ""
+luna_key = "5545963604:AAFaDB-N19p1c9mPBruvzScQPdiJqM-7x30"
 
 # Check readme for where to get key 
